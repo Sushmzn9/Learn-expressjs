5a6ef2377724c383de4e964b16191b41ab579acf
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-06-16 23:07:57 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [form, setForm] = useState({});
+
+  const handleForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/demo", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <p>{JSON.stringify(form)}</p>
+
+        <span>Username</span>
+        <input type="text" name="username" onChange={handleForm} />
+
+        <span>Password</span>
+        <input type="password" name="password" onChange={handleForm} />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
